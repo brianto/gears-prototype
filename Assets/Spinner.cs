3af29a100256827a64908e6170eeff90c2480662
Brian To <bxt5647@rit.edu>
2018-02-03 13:53:15 -0800

    Add spinner

---
+++ Assets/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+		
+    }
+	
+    void Update() {
+        this.transform.RotateAround(this.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
