2c6ab867184cf6b4d3dfab18cc1b10f8bd4933c5
Brian To <bxt5647@rit.edu>
2018-02-03 14:29:58 -0800

    Make spinners spin in op. directions

---
+++ Assets/RotationDriver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationDriver : MonoBehaviour {
+
+    public Spinner[] spinners;
+
+    public float speed;
+
+    void Start() {
+    }
+	
+    // Update is called once per frame
+    void Update() {
+        foreach (Spinner spinner in this.spinners) {
+            spinner.speed = this.speed;
+        }
+    }
+}
--- Assets/Spinner.cs
+++ Assets/Spinner.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spinner : MonoBehaviour {
 
+    // TODO gear ratio
+
+    private HashSet<Spinner> connected;
+
+    public float speed;
+
+    public Vector3 axis;
+
     // Use this for initialization
     void Start() {
-		
     }
 	
     void Update() {
-        this.transform.RotateAround(this.transform.position, Vector3.up, 20 * Time.deltaTime);
+        this.transform.RotateAround(this.transform.position, this.axis, this.speed * Time.deltaTime);
+    }
+
+    public void Connect(Spinner spinner) {
+        this.speed = spinner.speed;
+        this.axis *= -1;
     }
 }
